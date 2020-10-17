19e0db9fe2afb7d641b2ac183c770c071040def8
Saumyadip <saumyadip11@gmail.com>
2020-10-17 11:46:37 +0530

    Updated list

--- Fizz Buzz (improved).py
+++ Fizz Buzz (improved).py
@@ -1,12 +1,12 @@
-numbers = list(range(1,(100)+1))
+numbers = list(range(1, 100 + 1))  # Gives a list of numbers from 1 to 100.
 
-for i in numbers:
-    if i % 15 == 0:
+for i in numbers:   # 'i' as temp variable for numbers
+    if i % 15 == 0:  # if the element of a list is multiple of both 3 and 5 or (3*5) then it will return 0.
         print("Fizz Buzz")
 
-    elif i % 3 ==0:
+    elif i % 3 ==0:  # if the element of a list is multiple of 3 then it will return 0.
         print("Fizz")
-    elif i % 5 ==0:
+    elif i % 5 ==0:  # if the element of a list is multiple of 5 then it will return 0.
         print('Buzz')
 
     elif i > 1:
